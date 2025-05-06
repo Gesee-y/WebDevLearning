b7ee2da11f6a6609839ce4775cf5ea2baa12861d
Talom Laël <gesee37@gmail.com>
2025-05-06 17:56:56 +0100

    Update script.js

--- src/Day11/js/script.js
+++ src/Day11/js/script.js
@@ -1,5 +1,25 @@
-import ak from "server.js"
+/* The main script to manage super objects */
 
-function fetch_data(){
+const button = document.getElementById("start");
 
+button.addEventListener("click", () => onClick());
+button.addEventListener("hover", () => onHover());
+
+function onClick(){
+	changeCol(button, "white", "black");
+	setTimeout(() => button.style = "", 100);
+	goToPage("src/quiz.html");
+}
+
+function onHover(){
+	changeCol(button, "lime", "black");
+}
+
+function changeCol(b, c1, c2){
+	b.style.backgroundColor = c1;
+	b.style.color = c2;
+}
+
+function goToPage(url){
+	window.location.assign(url);
 }
